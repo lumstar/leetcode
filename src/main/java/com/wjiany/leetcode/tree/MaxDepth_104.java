db2ff24e7358457f29f8f6daa0542c1bfe385c6b
lumstar <open_to@163.com>
2024-03-17 20:15:02 +0800

    104

--- src/main/java/com/wjiany/leetcode/tree/MaxDepth_104.java
+++ src/main/java/com/wjiany/leetcode/tree/MaxDepth_104.java
@@ -16,4 +16,15 @@ public class MaxDepth_104 {
         treeDeep(node.right,deepAdd+1);
     }
 
+
+    public int maxDepth2(TreeNode root) {
+
+        return deep(root,0);
+    }
+    public int deep(TreeNode root,int deep){
+        if(root == null){
+            return deep;
+        }
+        return Math.max(deep(root.left,deep+1),deep(root.right,deep+1));
+    }
 }
